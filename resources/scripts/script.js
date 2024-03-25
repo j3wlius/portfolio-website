893221b9dd0f2353f90a77c8babec05cdf49e9bc
julius-tamale <juliotamex@proton.me>
2024-03-25 06:41:38 +0300

    Update portfolio website

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,23 +1,32 @@
-// ACCESS HTML ELEMENTS
-const hamburger = document.querySelector('.hamburger'); // hamburger
-const navMenu = document.querySelector('.nav-menu'); //nav menu
-const navLink = document.querySelectorAll('.nav-link'); // nav links
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+}
+
+// Close navbar when link is clicked
+const navLink = document.querySelectorAll('.nav-link');
+
+navLink.forEach((n) => n.addEventListener('click', closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}
+
+// Event Listeners: Handling toggle event
 const toggleSwitch = document.querySelector(
   '.theme-switch input[type="checkbox"]'
-); // toggle checkbox
+);
 
-// const lightToggle = document.querySelector('.fa-sun');
-
-// console.log(lightToggle);
-
-/* 
-    FUNCTION TO HANDLE THEME CHANGES 
-    AND STORE USER CHOICE 
-*/
 function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark');
+    localStorage.setItem('theme', 'dark'); // stores color theme on future visits
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
     localStorage.setItem('theme', 'light');
@@ -26,34 +35,23 @@ function switchTheme(e) {
 
 toggleSwitch.addEventListener('change', switchTheme, false);
 
-// SAVE USER PREFERENCE ON LOAD
+// Save user preference on load
+
 const currentTheme = localStorage.getItem('theme')
   ? localStorage.getItem('theme')
   : null;
 
 if (currentTheme) {
   document.documentElement.setAttribute('data-theme', currentTheme);
+
   if (currentTheme === 'dark') {
     toggleSwitch.checked = true;
   }
 }
 
-/* 
-    OPEN NAVIGATION MENU WHEN HAMBURGER
-    IS CLICKED
-*/
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
-});
+//Adding date
 
-/* 
-    CLOSE NAV HAMBURGER MENU WHEN A LINK 
-    IS CLICKED 
-*/
-navLink.forEach((link) =>
-  link.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-  })
-);
+let myDate = document.querySelector('#datee');
+
+const yes = new Date().getFullYear();
+myDate.innerHTML = yes;
