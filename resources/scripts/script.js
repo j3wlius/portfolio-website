7cf54a47d04408060d8b14c177040b3043d8b3b3
jewlius <tamalejulius5@gmail.com>
2024-04-27 11:15:23 +0300

    portfolio 3.0

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -3,3 +3,24 @@
 // update copyright year
 const currentDate = new Date();
 document.querySelector('.year').innerText = currentDate.getFullYear();
+
+// skills toggle
+
+const toggleBtns = document.querySelectorAll('.toggle-btn');
+const toggleBtnBox = document.querySelector('.skills-toggle');
+const skillsBox = document.querySelector(".skills-box");
+
+
+const elemToggleFunc = function (elem) {
+  elem.classList.toggle('active');
+};
+
+for (let i = 0; i < toggleBtns.length; i++) {
+  toggleBtns[i].addEventListener('click', function () {
+    elemToggleFunc(toggleBtnBox);
+    for (let i = 0; i < toggleBtns.length; i++) {
+      elemToggleFunc(toggleBtns[i]);
+    }
+    elemToggleFunc(skillsBox);
+  });
+}
