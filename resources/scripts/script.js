6df4f7b41d9b27beef12f1fc5ce7db67b813c86a
jewlius <tamalejulius5@gmail.com>
2024-04-28 11:46:04 +0300

    portfolio 3.0

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -44,3 +44,33 @@ for (let i = 0; i < navItemLinks.length; i++) {
     }
   });
 }
+
+const filterBtn = document.querySelectorAll('.filter-item');
+const itemCategory = document.querySelectorAll('.item-category');
+
+for (let i = 0; i < filterBtn.length; i++) {
+  filterBtn[i].addEventListener('click', function () {
+    // remove all active class from filter button
+    for (let i = 0; i < filterBtn.length; i++) {
+      filterBtn[i].classList.remove('active');
+    }
+    // added active class on filter button clicked
+    this.classList.add('active');
+
+    // show item, based on filter button click
+    for (let i = 0; i < itemCategory.length; i++) {
+      const itemCategoryText = itemCategory[i].textContent;
+      console.log(itemCategoryText);
+      switch (this.textContent) {
+        case itemCategoryText:
+          itemCategory[i].parentElement.classList.add('active');
+          break;
+        case 'All':
+          itemCategory[i].parentElement.classList.add('active');
+          break;
+        default:
+          itemCategory[i].parentElement.classList.remove('active');
+      }
+    }
+  });
+}
