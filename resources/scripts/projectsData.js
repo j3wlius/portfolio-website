908f794852af5fcdf544fc6b46f44fb17520267c
jewlius <tamalejulius5@gmail.com>
2024-06-27 21:23:01 +0300

    portfolio revamp

---
+++ resources/scripts/projectsData.js
@@ -0,0 +1,38 @@
+const projectsData = [
+	{
+		title: "Project 1",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada nunc vel risus commodo. Varius quam quisque id diam vel quam.",
+		technologies: ["HTML", "CSS", "JavaScript"],
+		image: "./resources/images/project/bitbytes.png",
+		link: "https://github.com",
+	},
+	{
+		title: "Project 2",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada nunc vel risus commodo. Varius quam quisque id diam vel quam.",
+		technologies: ["HTML", "CSS", "JavaScript"],
+		image: "./resources/images/project/bitbytes.png",
+		link: "https://github.com",
+	},
+	{
+		title: "Project 3",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada nunc vel risus commodo. Varius quam quisque id diam vel quam.",
+		technologies: ["HTML", "CSS", "JavaScript"],
+		image: "./resources/images/project-1.png",
+		link: "https://github.com",
+	},
+	{
+		title: "Project 4",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada nunc vel risus commodo. Varius quam quisque id diam vel quam.",
+		technologies: ["HTML", "CSS", "JavaScript"],
+		image: "./resources/images/project-1.png",
+		link: "https://github.com",
+	},
+];
+
+function handleProjectsData(data) {}
+
+export { projectsData, handleProjectsData };
