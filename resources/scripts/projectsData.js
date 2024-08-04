9114a37f5786c300310ada9082a815ec2586247f
Julio <tamalejulius5@gmail.com>
2024-08-04 19:25:16 +0300

    portfolio update

--- resources/scripts/projectsData.js
+++ resources/scripts/projectsData.js
@@ -4,35 +4,41 @@ const projectsData = [
 		description:
 			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada nunc vel risus commodo. Varius quam quisque id diam vel quam.",
 		technologies: ["HTML", "CSS", "JavaScript"],
+		categorty: "Full stack",
 		image: "./resources/images/project/bitbytes.png",
-		link: "https://github.com",
+		live: "https://github.com",
+		code: "https://github.com",
 	},
 	{
-		title: "Project 2",
+		title: "Project 1",
 		description:
 			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada nunc vel risus commodo. Varius quam quisque id diam vel quam.",
 		technologies: ["HTML", "CSS", "JavaScript"],
+		categorty: "Full stack",
 		image: "./resources/images/project/bitbytes.png",
-		link: "https://github.com",
+		live: "https://github.com",
+		code: "https://github.com",
 	},
 	{
-		title: "Project 3",
+		title: "Project 1",
 		description:
 			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada nunc vel risus commodo. Varius quam quisque id diam vel quam.",
 		technologies: ["HTML", "CSS", "JavaScript"],
-		image: "./resources/images/project-1.png",
-		link: "https://github.com",
+		categorty: "Full stack",
+		image: "./resources/images/project/bitbytes.png",
+		live: "https://github.com",
+		code: "https://github.com",
 	},
 	{
-		title: "Project 4",
+		title: "Project 1",
 		description:
 			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada nunc vel risus commodo. Varius quam quisque id diam vel quam.",
 		technologies: ["HTML", "CSS", "JavaScript"],
-		image: "./resources/images/project-1.png",
-		link: "https://github.com",
+		categorty: "Full stack",
+		image: "./resources/images/project/bitbytes.png",
+		live: "https://github.com",
+		code: "https://github.com",
 	},
 ];
 
-function handleProjectsData(data) {}
-
-export { projectsData, handleProjectsData };
+export default projectsData;
